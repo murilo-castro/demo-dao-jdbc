aa642c447512267f38440e25fe20b678e74042e5
Murilo Castro <murilo.65@gmail.com>
2025-05-23 20:56:10 -0300

    feat: Creating SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller Seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller Seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer SellerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findSellerById(Integer SellerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findSellersAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
